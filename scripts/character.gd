b87b5f6fec1bd1e6b74b5f810014e6d134639f1c
jonatas57 <jdjonatasduarte57@gmail.com>
2026-05-01 15:46:15 -0300

    feat: add gravity and jump

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,11 +1,18 @@
 extends CharacterBody2D
 
-@export var speed: float = 200.0
+const JUMP_VELOCITY = -800.0
+@export var speed: float = 400.0
 
 func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		self.velocity += get_gravity() * delta
+	else:
+		if Input.is_action_just_pressed("jump"):
+			self.velocity.y = JUMP_VELOCITY
+
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		self.velocity.x = direction * speed
