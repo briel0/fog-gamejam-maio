9d3386390affd0ff78c1d8b85c190aa33688be5d
jonatas57 <jdjonatasduarte57@gmail.com>
2026-05-02 00:35:19 -0300

    feat: add hud life counter

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,14 +1,28 @@
 extends Control
 
 @export var progressBar: ProgressBar
+@export var lifeBar: HBoxContainer
+@export var lifeIcon: Texture
 
 func _ready() -> void:
 	set_progress(0)
+	set_life(3)
 
 func _process(delta: float) -> void:
 	var progress = progressBar.value
 	progress += delta * 10
 	set_progress(progress)
+
+func set_life(life: int) -> void:
+	var childCount = lifeBar.get_child_count()
+	for i in range(childCount):
+		var child = lifeBar.get_child(childCount - 1 - i)
+		if child is TextureRect:
+			if life > 0:
+				child.texture = lifeIcon
+				life -= 1
+			else:
+				child.texture = null
 
 func set_progress(value: float) -> void:
 	progressBar.value = value
