f5049c0b2b4ce6ab82c8d038b740b7946ea6603e
Gabriel <g.abreu@aluno.ufabc.edu.br>
2026-05-02 00:17:16 -0300

    feat: adiciona mecanica de tiro, cooldown, e estrutura base de fases

---
+++ scripts/bullet.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@export var speed: float = 700.0
+var direction: int = 1 # 1 para direita, -1 para esquerda
+
+func _physics_process(delta: float) -> void:
+    position.x += speed * direction * delta
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+    queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+    # Colocar lógica de dano depois
+    queue_free()
--- scripts/character.gd
+++ scripts/character.gd
@@ -2,40 +2,60 @@ extends CharacterBody2D
 var hanging  = false
 var jumpable = true
 const JUMP_VELOCITY = -1200.0
+const BEAM_OFFSET = 250
+
 @export var speed: float = 400.0
 
-func _ready() -> void:
-	pass
-func disable_collision_temp():
-	set_collision_mask_value(1,false)
-	await get_tree().create_timer(0.3).timeout
-	set_collision_mask_value(1,true)
-	
-func _physics_process(delta: float) -> void:
-	#gravidade pq ta no ar
-	jumpable=true
-	if not is_on_floor() and not is_on_ceiling():
-		self.velocity += get_gravity() * delta
-		jumpable=false
-	#ta pendurado
-	
-	if is_on_ceiling():
-		if Input.is_action_just_pressed("up"):
-			position[1] -=250
-		if Input.is_action_just_pressed("down"):
-			self.velocity += get_gravity() * delta
-	elif  is_on_floor() and Input.is_action_just_pressed("down"):
-			position[1] +=250
-	
-	if jumpable and Input.is_action_just_pressed("jump"):
-		disable_collision_temp()
-		self.velocity.y = JUMP_VELOCITY
-	
-	
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		self.velocity.x = direction * speed
-	else:
-		self.velocity.x = move_toward(self.velocity.x, 0, speed)
+@export var bullet_scene: PackedScene
+@onready var muzzle = $Muzzle
+@onready var shoot_timer: Timer = $ShootTimer
 
-	move_and_slide()
+var last_facing: int = 1
+
+func handle_combat():
+    if Input.is_action_just_pressed("shoot") and shoot_timer.is_stopped():
+        shoot_timer.start()
+        var b = bullet_scene.instantiate()
+        get_tree().root.add_child(b)
+        b.position = muzzle.global_position
+        b.direction = last_facing
+
+func _ready() -> void:
+    pass
+    
+func disable_collision_temp():
+    set_collision_mask_value(1,false)
+    await get_tree().create_timer(0.3).timeout
+    set_collision_mask_value(1,true)
+    
+func _physics_process(delta: float) -> void:
+    #gravidade pq ta no ar
+    jumpable=true
+    if not is_on_floor() and not is_on_ceiling():
+        self.velocity += get_gravity() * delta
+        jumpable=false
+    #ta pendurado
+    
+    if is_on_ceiling():
+        if Input.is_action_just_pressed("up"):
+            position.y -= BEAM_OFFSET #Abreu -> "Criei uma const pra caso a altura do Beam mude"
+        if Input.is_action_just_pressed("down"):
+            self.velocity += get_gravity() * delta
+    elif  is_on_floor() and Input.is_action_just_pressed("down"):
+            position.y += BEAM_OFFSET
+    
+    if jumpable and Input.is_action_just_pressed("jump"):
+        disable_collision_temp()
+        self.velocity.y = JUMP_VELOCITY
+    
+    var direction := Input.get_axis("left", "right")
+    if direction: 
+        last_facing = sign(direction)
+        muzzle.position.x = abs(muzzle.position.x) * last_facing
+        
+        self.velocity.x = direction * speed
+    else:
+        self.velocity.x = move_toward(self.velocity.x, 0, speed)
+
+    move_and_slide()
+    handle_combat()          
